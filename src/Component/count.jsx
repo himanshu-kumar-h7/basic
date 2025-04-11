8e37d73b1b68e7c34eff5608b350ff1bacaddfc0
Himanshu <Himanshu@Himanshu-Macbook-Air.local>
2025-04-11 08:56:07 +0530

    Counter Commit

---
+++ src/Component/count.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+
+
+  console.log("Hello")
+  const increment = () => setCount(prev => prev + 1);
+  const decrement = () => setCount(prev => prev - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Count: {count}</h1>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset} style={{ margin: '0 1rem' }}>Reset</button>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+
+export default Counter;
